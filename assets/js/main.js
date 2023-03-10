1b453f08dad8be0fccc5c478463ee460c79e1443
lucaruboni <lucaruboni@gmail.com>
2023-03-10 16:59:19 +0100

    [add] fizz buzz fizzbuzz condition

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,26 @@ per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 Prima di partire a scrivere codice poniamoci qualche domanda:
 Come faccio a sapere se un numero è divisibile per un altro? */
 
-let fizzEl = String("fizz")
+let fizzEl
 
 
 
 
 for (let index=1; index <= 100 ; index++){
-    console.log(index)
+  
 
     if (index % 3==0){  
-        index = fizzEl
+        console.log("fizz");
     }
 
-    
-    
+    else if (index % 5==0){
+        console.log("buzz");
+    }
+
+    else if (index % 3==0 && index % 5==0){
+        console.log("fizzbuzz");
+    }
+
+    else (index)
+    console.log(index)
 }
