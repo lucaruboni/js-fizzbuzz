22e0b8ff26bf005bbddf841f458b7a7860fbd161
lucaruboni <lucaruboni@gmail.com>
2023-03-10 16:21:16 +0100

    [add] ticket push

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/* Scrivi un programma che stampi in console i numeri da 1 a 100 ma che:
+per i multipli di 3 stampi “Fizz” al posto del numero
+per i multipli di 5 stampi “Buzz”
+per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Come faccio a sapere se un numero è divisibile per un altro? */
+
+let fizzEl = String("fizz")
+
+
+
+
+for (let index=1; index <= 100 ; index++){
+    console.log(index)
+
+    if (index % 3==0){  
+        index = fizzEl
+    }
+
+    
+    
+}
