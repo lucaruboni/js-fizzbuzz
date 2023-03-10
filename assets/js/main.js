b3f435e962f1b30ecff1e68287df836b0985bf8b
lucaruboni <lucaruboni@gmail.com>
2023-03-10 17:54:13 +0100

    [add] insertadjacenthtml and his content

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,28 +5,46 @@ per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 Prima di partire a scrivere codice poniamoci qualche domanda:
 Come faccio a sapere se un numero è divisibile per un altro? */
 
-let fizzEl
 
+const ulEl = document.querySelector(".list_el");
+console.log(ulEl);
+
+let liEl;
+const fizz = String("fizz")
+console.log(fizz)
+const buzz = String("buzz")
+console
+const fizzbuzz = String("fizzbuzz")
 
 
 
 for (let index=1; index <= 100 ; index++){
-  
+    let listElement = `<li></li>`
 
     if (index % 3==0 && index % 5==0){
-        console.log("fizzbuzz");
-        
+        console.log(fizzbuzz)
+        listElement = `<li>${fizzbuzz}</li>`;
     }
+
     else if (index % 3==0){  
-        console.log("fizz");
+        console.log(fizz);
+        listElement = `<li>${fizz}</li>`;
     }
 
     else if (index % 5==0){
-        console.log("buzz");
+        console.log(buzz);
+        listElement = `<li>${buzz}</li>`;
+    }
+
+    else {
+    console.log(index)
+    listElement = `<li>${index}</li>`;
     }
 
 
-
-    else (index)
-    console.log(index)
+    
+  ulEl.insertAdjacentHTML("beforeend", listElement)
 }
+
+
+
